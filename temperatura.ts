6509503afa2a7a0d1691ae5fdc22e0c2e1fb7ce6
cam04 <ch385593@gmail.com>
2025-03-14 18:53:29 -0500

    cambio

--- temperatura.ts
+++ temperatura.ts
@@ -8,33 +8,35 @@ class Ciudad {
     }
 }
 
-class Analizartemperatura{
+class Analizartemperatura {
     datos: Ciudad[];
 
     constructor(datos: Ciudad[]) {
         this.datos = datos;
     }
 
-    analizar(): { ciudadmascaliente: string; ciudadmasfria: string; promedio: string } | string {
+    analizar(): { ciudadmascaliente: string; ciudadmasfria: string; promedio: number } | string {
         if (this.datos.length === 0) return "No hay datos disponibles";
 
         const ciudadmascaliente = this.datos.reduce((max, actual) => (actual.temperatura > max.temperatura ? actual : max));
         const ciudadmasfria = this.datos.reduce((min, actual) => (actual.temperatura < min.temperatura ? actual : min));
-        const promedio = this.datos.reduce((acc, actual) => acc + actual.temperatura, 0) / this.datos.length;
+        const promedio = Math.round(this.datos.reduce((acc, actual) => acc + actual.temperatura, 0) / this.datos.length);
 
         return {
             ciudadmascaliente: ciudadmascaliente.ciudad,
             ciudadmasfria: ciudadmasfria.ciudad,
-            promedio: promedio.toFixed(2)
+            promedio: promedio
         };
     }
 }
 
-
 const ciudades = [
     new Ciudad("Bogotá", 18),
-    new Ciudad("cali", 50),
-    new Ciudad("Medellín", 24)
+    new Ciudad("Cali", 30),
+    new Ciudad("Medellín", 24),
+    new Ciudad("Popayán", 25),
+    new Ciudad("Quindío", 20),
+    new Ciudad("Valledupar", 44)
 ];
 
 const analizador = new Analizartemperatura(ciudades);
